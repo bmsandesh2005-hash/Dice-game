32f563ca80056a34ea921bf2fc6b116dd483619d
bmsandesh2005-hash <bmsandesh2005@gmail.com>
2026-02-25 19:25:46 +0530

    Add files via upload

---
+++ die_game.py
@@ -0,0 +1,13 @@
+import random
+print("Welcome to the game of rolling dice.")
+while True:
+    choice=input("press enter the dice or 'q' to quit.")
+    if choice=='q':
+        print("Thank you for playing")
+        break
+    elif choice=='':
+        number=random.randint(1,6)
+        print(f"your number is {number}")
+    else:
+         print("wrong choice")
+print("Game OVER")
